b29631597d67d94672080a3f4bcf55dd6451c91e
Ana Matoso <anamatoso92@gmail.com>
2022-01-16 17:01:27 +0000

    sfv

---
+++ populate.sql
@@ -0,0 +1,230 @@
+-- Populate country
+insert into country values('PRT', 'Portugal','https://upload.wikimedia.org/wikipedia/commons/thumb/5/5c/Flag_of_Portugal.svg/255px-Flag_of_Portugal.svg.png');
+insert into country values('ZAF', 'South Africa','https://upload.wikimedia.org/wikipedia/commons/thumb/a/af/Flag_of_South_Africa.svg/1280px-Flag_of_South_Africa.svg.png');
+insert into country values('ESP', 'Spain','https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Flag_of_Spain.svg/2560px-Flag_of_Spain.svg.png');
+insert into country values('USA', 'United States','https://upload.wikimedia.org/wikipedia/commons/a/a4/Flag_of_the_United_States.svg');
+insert into country values('AUS', 'Australia','https://upload.wikimedia.org/wikipedia/commons/thumb/8/88/Flag_of_Australia_%28converted%29.svg/1024px-Flag_of_Australia_%28converted%29.svg.png');
+insert into country values('JPN', 'Japan','https://upload.wikimedia.org/wikipedia/commons/thumb/9/9e/Flag_of_Japan.svg/800px-Flag_of_Japan.svg.png');
+insert into country values('BRA', 'Brazil','https://upload.wikimedia.org/wikipedia/commons/thumb/a/a2/Flag_of_Brazil_%283-2%29.svg/2560px-Flag_of_Brazil_%283-2%29.svg.png');
+insert into country values('ARG', 'Argentina','https://upload.wikimedia.org/wikipedia/commons/thumb/1/1a/Flag_of_Argentina.svg/1280px-Flag_of_Argentina.svg.png');
+insert into country values('ISL', 'Iceland','https://upload.wikimedia.org/wikipedia/commons/thumb/c/ce/Flag_of_Iceland.svg/1250px-Flag_of_Iceland.svg.png');
+insert into country values('ITA', 'Italy','https://upload.wikimedia.org/wikipedia/commons/thumb/0/03/Flag_of_Italy.svg/2560px-Flag_of_Italy.svg.png');
+insert into country values('GBR', 'United Kingdom','https://upload.wikimedia.org/wikipedia/commons/thumb/f/f2/Flag_of_Great_Britain_%281707–1800%29.svg/2560px-Flag_of_Great_Britain_%281707–1800%29.svg.png');
+insert into country values('GRC', 'Greece','https://upload.wikimedia.org/wikipedia/commons/thumb/5/5c/Flag_of_Greece.svg/1280px-Flag_of_Greece.svg.png');
+insert into country values('MOZ', 'Mozambique','https://upload.wikimedia.org/wikipedia/commons/thumb/d/d0/Flag_of_Mozambique.svg/2560px-Flag_of_Mozambique.svg.png');
+insert into country values('ARE', 'United Arab Emirates','https://upload.wikimedia.org/wikipedia/commons/thumb/c/cb/Flag_of_the_United_Arab_Emirates.svg/1280px-Flag_of_the_United_Arab_Emirates.svg.png');
+insert into country values('CAN', 'Canada','https://upload.wikimedia.org/wikipedia/commons/d/d9/Flag_of_Canada_%28Pantone%29.svg');
+
+
+-- Populate Location
+insert into location values (38.705276,9.146657,'Lisbon','PRT'); insert into marina values (38.705276,9.146657);
+insert into location values (-33.924870,18.424055,'Cape Town','ZAF'); insert into port values (-33.924870,18.424055);
+insert into location values (41.157844,-8.629105,'Porto','PRT'); insert into wharf values (41.157844,-8.629105);
+insert into location values (-29.858681,31.021839,'Durban','ZAF'); insert into marina values (-29.858681,31.021839);
+insert into location values (41.385063,2.173404,'Barcelona','ESP'); insert into port values (41.385063,2.173404);
+insert into location values (25.764144,-80.057571,'Miami','USA'); insert into wharf values (25.764144,-80.057571);
+insert into location values (-31.950527,115.860458,'Perth','AUS'); insert into marina values (-31.950527,115.860458);
+insert into location values (34.693737,135.502167,'Osaka','JPN'); insert into marina values (34.693737,135.502167);
+insert into location values (-22.906847,-43.172897,'Rio de Janeiro','BRA'); insert into port values (-22.906847,-43.172897);
+insert into location values (-34.603683,-58.381557,'Buenos Aires','ARG'); insert into port values (-34.603683,-58.381557);
+insert into location values (64.128288,-21.827774,'Reykjavik','ISL'); insert into wharf values (64.128288,-21.827774);
+insert into location values (40.839980,14.252540,'Naples','ITA'); insert into wharf values (40.839980,14.252540);
+insert into location values (41.771530,12.230000,'Fuimicino','ITA'); insert into marina values (41.771530,12.230000);
+insert into location values (28.521076,-81.465523,'Valencia','ESP'); insert into port values (28.521076,-81.465523);
+insert into location values (-33.961302,25.662782,'Port Elizabeth','ZAF'); insert into wharf values (-33.961302,25.662782);
+insert into location values (32.6496497,-16.9086783,'Funchal','PRT'); insert into port values (32.6496497,-16.9086783);
+insert into location values (32.7174202,-117.1627728,'San Diego','USA'); insert into port values (32.7174202,-117.1627728);
+insert into location values (-34.9281805,138.5999312,'Adelaide','AUS'); insert into marina values (-34.9281805,138.5999312);
+insert into location values (-23.5506507,-46.6333824,'São Paulo','BRA'); insert into marina values (-23.5506507,-46.6333824);
+insert into location values (45.4371908,12.3345898,'Venice','ITA'); insert into wharf values (45.4371908,12.3345898);
+insert into location values (40.147266,-8.860761,'Figueira da Foz','PRT'); insert into marina values (40.147266, -8.860761);
+insert into location values (40.635114,-8.651154,'Aveiro','PRT'); insert into port values (40.635114, -8.651154);
+insert into location values (42.239086,-8.731343,'Vigo','ESP'); insert into wharf values (42.239086, -8.731343);
+insert into location values (40.703546,-74.006764,'New York','USA'); insert into marina values (40.703546, -74.006764);
+insert into location values (-33.860549,151.211336,'Sidney','AUS'); insert into port values (-33.860549, 151.211336);
+insert into location values (35.652305,139.761995,'Tokyo','JPN'); insert into wharf values (35.652305, 139.761995);
+insert into location values (-12.952183,-38.503131,'Salvador','BRA'); insert into marina values (-12.952183, -38.503131);
+insert into location values (-38.046028,-57.536806,'Mar del Plata','ARG'); insert into port values (-38.046028, -57.536806);
+insert into location values (33.738706,-118.266112,'Los Angeles','USA'); insert into wharf values (33.738706, -118.266112);
+insert into location values (37.799968,-122.398026,'San Francisco','USA'); insert into marina values (37.799968, -122.398026);
+insert into location values (51.4816546,-3.1791934, 'Cardiff', 'GBR'); insert into marina values (51.4816546,-3.1791934);
+insert into location values (50.8214626,-0.1400561, 'Brighton', 'GBR'); insert into marina values (50.8214626,-0.1400561);
+insert into location values (37.9839412,23.7283052, 'Athens', 'GRC'); insert into port values (37.9839412,23.7283052);
+insert into location values (37.0377582,22.1109392, 'Kalamata', 'GRC'); insert into wharf values (37.0377582,22.1109392);
+insert into location values (-25.966213,32.56745, 'Maputo', 'MOZ'); insert into port values (-25.966213,32.56745);
+insert into location values (25.2653471,55.2924914, 'Dubai', 'ARE'); insert into marina values (25.2653471,55.2924914);
+insert into location values (24.4538352,54.3774014, 'Abu Dhabi', 'ARE'); insert into marina values (24.4538352,54.3774014);
+insert into location values (43.6534817,-79.3839347, 'Toronto', 'CAN'); insert into marina values (43.6534817,-79.3839347);
+
+
+--Populate Person
+insert into person values (126578,'Teresa Villaverde','PRT');
+insert into person values (126578,'Martin Scorsese','ZAF');
+insert into person values (5679890,'Tim Burton','USA');
+insert into person values (456890,'Steven Spielberg','BRA');
+insert into person values (88904123,'Quentin Tarantino','ARG');
+insert into person values (908765,'Pedro Almodovar','ESP');
+insert into person values (14617319,'Christopher Nolan','AUS');
+insert into person values (55667,'Hayao Miazaki','JPN');
+insert into person values (68904,'Stanley Kubrick','ISL');
+insert into person values (1345687,'Federico Fellini','ITA');
+insert into person values (2956612,'Marta Silva','PRT'); --sailor
+insert into person values (1962857,'Martim Vincente','ESP'); --sailor
+insert into person values (4850294,'Carlos Ruiz','ARG'); --owner
+insert into person values (1046205,'Diogo Silva','BRA'); --owner
+insert into person values (0193758,'Lewis Button','AUS'); --sailor
+insert into person values (2957392,'Michael Clifford','AUS'); --sailor
+insert into person values (2045869,'Chris Hoeven','AUS'); --sailor
+insert into person values (1220588,'Haruki Murakami','JPN'); --owner & sailor
+insert into person values (2000582,'Charles Kubica','ISL'); --owner
+insert into person values (2945190,'Alfredo Giovinazzi','ITA'); --owner
+insert into person values (2945190,'Ana Rendeiro','PRT');
+insert into person values (2945191,'João Rendeiro','PRT');
+insert into person values (8435738,'Maria Joaquina','PRT');
+insert into person values (34678,'Nelson Mandela','ZAF');
+insert into person values (27368,'James Cameron','USA');
+insert into person values (8873768,'Luana Santana','BRA');
+insert into person values (3824,'Pablo Alboran','ARG');
+insert into person values (8379268,'Rodrigo Santoro','ESP');
+insert into person values (89347874,'Nicole Kidman','AUS');
+insert into person values (2934729,'Fusajiro Yamauchi','JPN');
+insert into person values (238947,'Jamie Dornan','ISL');
+insert into person values (23874876,'Luigi Ferrari','ITA');
+insert into person values (38596665,'Armando Stroll','CAN'); --sailor
+insert into person values (10386720,'Albert Eisntein','ARE'); --sailor
+insert into person values (10033958,'Ahmad Salem','ARE'); --sailor
+insert into person values (10049682,'Matias Damasio','MOZ'); --sailor
+insert into person values (58995496,'Nicholas Sparks','GBR'); --sailor
+insert into person values (22059683,'Carlos Sainz','ESP'); --owner
+insert into person values (50025860,'Gregori Mattos','GRC'); --owner
+insert into person values (13359683,'João Silva','PRT'); --owner
+insert into person values (22058689,'John Piastri','ZAF'); --owner & sailor
+insert into person values (20566893,'Lucas Smith','GBR'); --owner & sailor
+
+
+--Populate Sailor
+insert into sailor values (126578,'PRT');
+insert into sailor values (5679890,'USA');
+insert into sailor values (456890,'BRA');
+insert into sailor values (88904123,'ARG');
+insert into sailor values (908765,'ESP');
+insert into sailor values (2956612,'PRT');
+insert into sailor values (1962857,'ESP');
+insert into sailor values (0193758,'AUS');
+insert into sailor values (2957392,'AUS');
+insert into sailor values (2045869,'AUS');
+insert into sailor values (8435738,'PRT');
+insert into sailor values (34678,'ZAF');
+insert into sailor values (27368,'USA');
+insert into sailor values (8379268,'ESP');
+insert into sailor values (89347874,'AUS');
+insert into sailor values (2934729,'JPN');
+insert into sailor values (238947,'ISL');
+insert into sailor values (23874876,'ITA');
+insert into sailor values (1220588, 'JPN');
+insert into sailor values (38596665,'CAN');
+insert into sailor values (10386720,'ARE');
+insert into sailor values (10033958,'ARE');
+insert into sailor values (10049682,'MOZ');
+insert into sailor values (58995496,'GBR');
+insert into sailor values (22058689,'ZAF');
+insert into sailor values (20566893,'GBR');
+
+--Populate Owner
+insert into owner values (126578,'ZAF','1963-08-01');
+insert into owner values (5679890,'USA','1977-03-31');
+insert into owner values (456890,'BRA','1945-12-05');
+insert into owner values (14617319,'AUS','1974-09-11');
+insert into owner values (55667,'JPN','1961-01-01');
+insert into owner values (68904,'ISL','1953-08-09');
+insert into owner values (1345687,'ITA','1963-08-01');
+insert into owner values (2000582,'ISL', '1999-09-16');
+insert into owner values (2945190,'ITA', '1996-03-01');
+insert into owner values (4850294,'ARG', '1971-10-08');
+insert into owner values (1046205,'BRA', '1985-10-10');
+insert into owner values (8873768,'BRA','1945-01-12');
+insert into owner values (3824,'ARG','1987-02-20');
+insert into owner values (8379268,'ESP','1955-03-11');
+insert into owner values (89347874,'AUS','1965-04-21');
+insert into owner values (2945190,'PRT','1980-07-30');
+insert into owner values (1220588,'JPN','1984-11-30');
+insert into owner values (22059683,'ESP','1954-12-13');
+insert into owner values (50025860,'GRC','1994-09-30');
+insert into owner values (13359683,'PRT','2000-06-23');
+insert into owner values (22058689,'ZAF','2001-04-14');
+insert into owner values (20566893,'GBR','1998-02-17');
+insert into owner values (2945191,'PRT','1955-04-23');
+
+--Populate Boat
+insert into boat values ('PRT', 98237,2000,40,'O Desconhecido','BRA',8873768);
+insert into boat values ('ARG', 839,2001,40,'El Rey','ARG',3824);
+insert into boat values ('PRT', 23748,2012,30,'EL Hombre','ESP',8379268);
+insert into boat values ('AUS', 23784,2020,20,'The Great','AUS',89347874);
+insert into boat values ('ZAF', 58947,2021,50,'The Inocent','PRT',2945190);
+insert into boat values ('ITA', 18293,2007,50,'Breeza','ZAF',126578); --vhf
+insert into boat values ('PRT', 20586,2016,60,'Avante','ITA',1345687); --vhf
+insert into boat values ('AUS', 10083,2015,20,'Antelope','ARG',4850294); --vhf
+insert into boat values ('ISL', 11940,2004,45,'Freedom','JPN',1220588);
+insert into boat values ('ZAF', 49602,1999,40,'Seas the Day','ARG',3824); --vhf
+insert into boat values ('CAN', 58201,2001,40,'Carpe Diem','ESP',22059683);
+insert into boat values ('USA', 30699,1990,50,'Blue Moon','GRC',50025860);
+insert into boat values ('GBR', 10059,2015,20,'Escape','ZAF',22058689);
+insert into boat values ('CAN', 20956,1984,45,'Liberdade','PRT',13359683); --vhf
+insert into boat values ('GBR', 14296,2019,55,'Serendipity','AUS',89347874); --vhf
+insert into boat values ('ZAF', 23874682,2021,100,'Conspirasea','PRT',2945191);
+insert into boat values ('GBR', 93476,2021,40,'Black Pearl','USA',5679890);
+insert into boat values ('PRT', 345,2021,40,'The Flying Dutchman','BRA',456890);
+insert into boat values ('AUS', 3532,2021,40,'The Queen Annes Revenge','AUS',14617319);
+insert into boat values ('JPN', 4352,2021,40,'The Dying Gull','JPN',55667);
+insert into boat values ('ISL', 346547,2021,40,'Empress','ISL',68904);
+insert into boat values ('ISL', 848934,2021,40,'Wicked Wench','ISL',2000582);
+insert into boat values ('ITA', 93287,2021,40,'Pirate Armada','ITA',2945190);
+insert into boat values ('BRA', 28736,2021,40,'Jolly Roger','BRA',1046205);
+insert into boat values ('GBR', 8367,2021,40,'The Barnacle','GBR',20566893);
+
+
+--Populate Boat with VHF
+insert into boatwithvhf values ('ITA', 18293, 294826586);
+insert into boatwithvhf values ('PRT', 20586, 937573910);
+insert into boatwithvhf values ('AUS', 10083, 184794396);
+insert into boatwithvhf values ('CAN', 20956, 194739576);
+insert into boatwithvhf values ('GBR', 14296, 182958928);
+insert into boatwithvhf values ('ZAF', 49602, 120069305);
+
+--Populate Schedule
+insert into schedule values('2021-12-30','2022-01-06');
+insert into schedule values('2021-12-30','2021-12-31');
+insert into schedule values('2021-12-23','2021-12-26');
+insert into schedule values('2021-12-30','2022-01-30');
+insert into schedule values('2020-07-30','2020-08-10');
+insert into schedule values('2022-08-10','2022-08-25');
+
+--Populate Reservation
+insert into reservation values('PRT',98237,'PRT',8435738,'2021-12-30','2022-01-06');
+insert into reservation values('ARG',839,'ITA',23874876,'2021-12-30','2021-12-31');
+insert into reservation values('PRT',23748,'AUS',89347874,'2021-12-30','2021-12-31');
+insert into reservation values('AUS',23784,'JPN',2934729,'2021-12-30','2022-01-30');
+insert into reservation values('ZAF',49602,'JPN',1220588,'2020-07-30','2020-08-10');
+insert into reservation values('GBR', 14296,'CAN',38596665,'2022-08-10','2022-08-25');
+
+--Populate Trip
+-- maria joaquina vai n'o desconhecido numa reserva de 8 dias com 2 trips: tokyo-sidney e sidney-sao paulo
+insert into trip values('2021-12-30',interval '1 day','PRT',98237,'PRT',8435738,'2021-12-30','2022-01-06', 35.652305, 139.761995,-33.860549, 151.211336);
+insert into trip values('2021-12-31',interval '6 days','PRT',98237,'PRT',8435738,'2021-12-30','2022-01-06', -33.860549, 151.211336,-23.5506507,-46.6333824);
+-- Luigi ferrari vai no El Rey numa reserva de 2 dias com 1 trip: naples-barcelona
+insert into trip values('2021-12-30',interval '1 day','ARG',839,'ITA',23874876,'2021-12-30','2021-12-31', 40.839980,14.252540,41.385063,2.173404);
+-- nicole kidman vai no El Hombre numa reserva de 2 dias com 1 trip: Reykjavik-Funchal
+insert into trip values('2021-12-30',interval '1 day','PRT',23748,'AUS',89347874,'2021-12-30','2021-12-31', 64.128288,-21.827774,32.6496497,-16.9086783);
+-- Fusajiro Yamauchi vai no the great numa reserva de 53 dias com 3 trips: NY-Aveiro, aveiro-cape town, cape town-adelaide
+insert into trip values('2021-12-30',interval '2 days','AUS',23784,'JPN',2934729,'2021-12-30','2022-01-30', 40.703546, -74.006764,40.635114, -8.651154);
+insert into trip values('2022-01-01',interval '10 days','AUS',23784,'JPN',2934729,'2021-12-30','2022-01-30',40.635114, -8.651154,-33.924870,18.424055);
+insert into trip values('2022-01-11',interval '19 days','AUS',23784,'JPN',2934729,'2021-12-30','2022-01-30', -33.924870,18.424055,-34.9281805,138.5999312);
+-- haruki murakami vai no seas the day durante 12 dias com duas trips: lisbon-funchal & adelaide-perth
+insert into trip values('2020-07-30',interval '2 day','ZAF',49602,'JPN',1220588,'2020-07-30','2020-08-10', 38.705276,9.146657,32.6496497,-16.9086783);
+insert into trip values('2020-08-05',interval '4 day','ZAF',49602,'JPN',1220588,'2020-07-30','2020-08-10', -34.9281805,138.5999312,-31.950527,115.860458);
+-- armando stroll no boat Serendipity reserva de 15 dias com 4 trips: barcelona-lisboa, valencia-reykjavik, athens-kalamata & los angeles-san francisco
+insert into trip values('2022-08-10',interval '3 day','GBR', 14296,'CAN',38596665,'2022-08-10','2022-08-25', 41.385063,2.173404,38.705276,9.146657);
+insert into trip values('2022-08-13',interval '2 day','GBR', 14296,'CAN',38596665,'2022-08-10','2022-08-25', 28.521076,-81.465523,64.128288,-21.827774);
+insert into trip values('2022-08-15',interval '2 day','GBR', 14296,'CAN',38596665,'2022-08-10','2022-08-25', 37.9839412,23.7283052,37.0377582,22.1109392);
+insert into trip values('2022-08-20',interval '2 day','GBR', 14296,'CAN',38596665,'2022-08-10','2022-08-25', 33.738706, -118.266112,37.799968, -122.398026);
+
