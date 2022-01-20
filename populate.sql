2d0e3f52c2552413d44da38e4585a3e8ddf629cc
inessarana <inessarana@gmail.com>
2022-01-20 10:53:24 +0000

    correct populate location, person, owner, sailor

--- populate.sql
+++ populate.sql
@@ -16,144 +16,144 @@ insert into country values('tinyurl.com/39zdcjaj','United Arab Emirates','AE');
 insert into country values('tinyurl.com/4n6tdfba','Canada','CA');
 
 
--- Populate Location
-insert into location values (38.705276,9.146657,'Lisbon','PRT'); insert into marina values (38.705276,9.146657);
-insert into location values (-33.924870,18.424055,'Cape Town','ZAF'); insert into port values (-33.924870,18.424055);
-insert into location values (41.157844,-8.629105,'Porto','PRT'); insert into wharf values (41.157844,-8.629105);
-insert into location values (-29.858681,31.021839,'Durban','ZAF'); insert into marina values (-29.858681,31.021839);
-insert into location values (41.385063,2.173404,'Barcelona','ESP'); insert into port values (41.385063,2.173404);
-insert into location values (25.764144,-80.057571,'Miami','USA'); insert into wharf values (25.764144,-80.057571);
-insert into location values (-31.950527,115.860458,'Perth','AUS'); insert into marina values (-31.950527,115.860458);
-insert into location values (34.693737,135.502167,'Osaka','JPN'); insert into marina values (34.693737,135.502167);
-insert into location values (-22.906847,-43.172897,'Rio de Janeiro','BRA'); insert into port values (-22.906847,-43.172897);
-insert into location values (-34.603683,-58.381557,'Buenos Aires','ARG'); insert into port values (-34.603683,-58.381557);
-insert into location values (64.128288,-21.827774,'Reykjavik','ISL'); insert into wharf values (64.128288,-21.827774);
-insert into location values (40.839980,14.252540,'Naples','ITA'); insert into wharf values (40.839980,14.252540);
-insert into location values (41.771530,12.230000,'Fuimicino','ITA'); insert into marina values (41.771530,12.230000);
-insert into location values (28.521076,-81.465523,'Valencia','ESP'); insert into port values (28.521076,-81.465523);
-insert into location values (-33.961302,25.662782,'Port Elizabeth','ZAF'); insert into wharf values (-33.961302,25.662782);
-insert into location values (32.6496497,-16.9086783,'Funchal','PRT'); insert into port values (32.6496497,-16.9086783);
-insert into location values (32.7174202,-117.1627728,'San Diego','USA'); insert into port values (32.7174202,-117.1627728);
-insert into location values (-34.9281805,138.5999312,'Adelaide','AUS'); insert into marina values (-34.9281805,138.5999312);
-insert into location values (-23.5506507,-46.6333824,'São Paulo','BRA'); insert into marina values (-23.5506507,-46.6333824);
-insert into location values (45.4371908,12.3345898,'Venice','ITA'); insert into wharf values (45.4371908,12.3345898);
-insert into location values (40.147266,-8.860761,'Figueira da Foz','PRT'); insert into marina values (40.147266, -8.860761);
-insert into location values (40.635114,-8.651154,'Aveiro','PRT'); insert into port values (40.635114, -8.651154);
-insert into location values (42.239086,-8.731343,'Vigo','ESP'); insert into wharf values (42.239086, -8.731343);
-insert into location values (40.703546,-74.006764,'New York','USA'); insert into marina values (40.703546, -74.006764);
-insert into location values (-33.860549,151.211336,'Sidney','AUS'); insert into port values (-33.860549, 151.211336);
-insert into location values (35.652305,139.761995,'Tokyo','JPN'); insert into wharf values (35.652305, 139.761995);
-insert into location values (-12.952183,-38.503131,'Salvador','BRA'); insert into marina values (-12.952183, -38.503131);
-insert into location values (-38.046028,-57.536806,'Mar del Plata','ARG'); insert into port values (-38.046028, -57.536806);
-insert into location values (33.738706,-118.266112,'Los Angeles','USA'); insert into wharf values (33.738706, -118.266112);
-insert into location values (37.799968,-122.398026,'San Francisco','USA'); insert into marina values (37.799968, -122.398026);
-insert into location values (51.4816546,-3.1791934, 'Cardiff', 'GBR'); insert into marina values (51.4816546,-3.1791934);
-insert into location values (50.8214626,-0.1400561, 'Brighton', 'GBR'); insert into marina values (50.8214626,-0.1400561);
-insert into location values (37.9839412,23.7283052, 'Athens', 'GRC'); insert into port values (37.9839412,23.7283052);
-insert into location values (37.0377582,22.1109392, 'Kalamata', 'GRC'); insert into wharf values (37.0377582,22.1109392);
-insert into location values (-25.966213,32.56745, 'Maputo', 'MOZ'); insert into port values (-25.966213,32.56745);
-insert into location values (25.2653471,55.2924914, 'Dubai', 'ARE'); insert into marina values (25.2653471,55.2924914);
-insert into location values (24.4538352,54.3774014, 'Abu Dhabi', 'ARE'); insert into marina values (24.4538352,54.3774014);
-insert into location values (43.6534817,-79.3839347, 'Toronto', 'CAN'); insert into marina values (43.6534817,-79.3839347);
+-- Populate Location (correct)
+insert into location values ('Lisbon', 38.705276,9.146657,'PT'); insert into marina values (38.705276,9.146657);
+insert into location values ('Cape Town',-33.924870,18.424055,'ZA'); insert into port values (-33.924870,18.424055);
+insert into location values ('Porto',41.157844,-8.629105,'PT'); insert into wharf values (41.157844,-8.629105);
+insert into location values ('Durban',-29.858681,31.021839,'ZA'); insert into marina values (-29.858681,31.021839);
+insert into location values ('Barcelona',41.385063,2.173404,'ES'); insert into port values (41.385063,2.173404);
+insert into location values ('Miami',25.764144,-80.057571,'US'); insert into wharf values (25.764144,-80.057571);
+insert into location values ('Perth',-31.950527,115.860458,'AU'); insert into marina values (-31.950527,115.860458);
+insert into location values ('Osaka',34.693737,135.502167,'JP'); insert into marina values (34.693737,135.502167);
+insert into location values ('Rio de Janeiro',-22.906847,-43.172897,'BR'); insert into port values (-22.906847,-43.172897);
+insert into location values ('Buenos Aires',-34.603683,-58.381557,'AR'); insert into port values (-34.603683,-58.381557);
+insert into location values ('Reykjavik',64.128288,-21.827774,'IS'); insert into wharf values (64.128288,-21.827774);
+insert into location values ('Naples',40.839980,14.252540,'IT'); insert into wharf values (40.839980,14.252540);
+insert into location values ('Fuimicino',41.771530,12.230000,'IT'); insert into marina values (41.771530,12.230000);
+insert into location values ('Valencia',28.521076,-81.465523,'ES'); insert into port values (28.521076,-81.465523);
+insert into location values ('Port Elizabeth',-33.961302,25.662782,'ZA'); insert into wharf values (-33.961302,25.662782);
+insert into location values ('Funchal',32.6496497,-16.9086783,'PT'); insert into port values (32.6496497,-16.9086783);
+insert into location values ('San Diego',32.7174202,-117.1627728,'US'); insert into port values (32.7174202,-117.1627728);
+insert into location values ('Adelaide',-34.9281805,138.5999312,'AU'); insert into marina values (-34.9281805,138.5999312);
+insert into location values ('São Paulo',-23.5506507,-46.6333824,'BR'); insert into marina values (-23.5506507,-46.6333824);
+insert into location values ('Venice',45.4371908,12.3345898,'IT'); insert into wharf values (45.4371908,12.3345898);
+insert into location values ('Figueira da Foz',40.147266,-8.860761,'PT'); insert into marina values (40.147266, -8.860761);
+insert into location values ('Aveiro',40.635114,-8.651154,'PT'); insert into port values (40.635114, -8.651154);
+insert into location values ('Vigo',42.239086,-8.731343,'ES'); insert into wharf values (42.239086, -8.731343);
+insert into location values ('New York',40.703546,-74.006764,'US'); insert into marina values (40.703546, -74.006764);
+insert into location values ('Sidney',-33.860549,151.211336,'AU'); insert into port values (-33.860549, 151.211336);
+insert into location values ('Tokyo',35.652305,139.761995,'JP'); insert into wharf values (35.652305, 139.761995);
+insert into location values ('Salvador',-12.952183,-38.503131,'BR'); insert into marina values (-12.952183, -38.503131);
+insert into location values ('Mar del Plata',-38.046028,-57.536806,'AR'); insert into port values (-38.046028, -57.536806);
+insert into location values ('Los Angeles',33.738706,-118.266112,'US'); insert into wharf values (33.738706, -118.266112);
+insert into location values ('San Francisco',37.799968,-122.398026,'US'); insert into marina values (37.799968, -122.398026);
+insert into location values ('Cardiff',51.4816546,-3.1791934,'GB'); insert into marina values (51.4816546,-3.1791934);
+insert into location values ('Brighton',50.8214626,-0.1400561, 'GB'); insert into marina values (50.8214626,-0.1400561);
+insert into location values ('Athens',37.9839412,23.7283052,'GR'); insert into port values (37.9839412,23.7283052);
+insert into location values ('Kalamata',37.0377582,22.1109392,'GR'); insert into wharf values (37.0377582,22.1109392);
+insert into location values ('Maputo',-25.966213,32.56745,'MZ'); insert into port values (-25.966213,32.56745);
+insert into location values ('Dubai',25.2653471,55.2924914,'AE'); insert into marina values (25.2653471,55.2924914);
+insert into location values ('Abu Dhabi',24.4538352,54.3774014,'AE'); insert into marina values (24.4538352,54.3774014);
+insert into location values ('Toronto',43.6534817,-79.3839347,'CA'); insert into marina values (43.6534817,-79.3839347);
 
 
---Populate Person
-insert into person values (126578,'Teresa Villaverde','PRT');
-insert into person values (126578,'Martin Scorsese','ZAF');
-insert into person values (5679890,'Tim Burton','USA');
-insert into person values (456890,'Steven Spielberg','BRA');
-insert into person values (88904123,'Quentin Tarantino','ARG');
-insert into person values (908765,'Pedro Almodovar','ESP');
-insert into person values (14617319,'Christopher Nolan','AUS');
-insert into person values (55667,'Hayao Miazaki','JPN');
-insert into person values (68904,'Stanley Kubrick','ISL');
-insert into person values (1345687,'Federico Fellini','ITA');
-insert into person values (2956612,'Marta Silva','PRT'); --sailor
-insert into person values (1962857,'Martim Vincente','ESP'); --sailor
-insert into person values (4850294,'Carlos Ruiz','ARG'); --owner
-insert into person values (1046205,'Diogo Silva','BRA'); --owner
-insert into person values (0193758,'Lewis Button','AUS'); --sailor
-insert into person values (2957392,'Michael Clifford','AUS'); --sailor
-insert into person values (2045869,'Chris Hoeven','AUS'); --sailor
-insert into person values (1220588,'Haruki Murakami','JPN'); --owner & sailor
-insert into person values (2000582,'Charles Kubica','ISL'); --owner
-insert into person values (2945190,'Alfredo Giovinazzi','ITA'); --owner
-insert into person values (2945190,'Ana Rendeiro','PRT');
-insert into person values (2945191,'João Rendeiro','PRT');
-insert into person values (8435738,'Maria Joaquina','PRT');
-insert into person values (34678,'Nelson Mandela','ZAF');
-insert into person values (27368,'James Cameron','USA');
-insert into person values (8873768,'Luana Santana','BRA');
-insert into person values (3824,'Pablo Alboran','ARG');
-insert into person values (8379268,'Rodrigo Santoro','ESP');
-insert into person values (89347874,'Nicole Kidman','AUS');
-insert into person values (2934729,'Fusajiro Yamauchi','JPN');
-insert into person values (238947,'Jamie Dornan','ISL');
-insert into person values (23874876,'Luigi Ferrari','ITA');
-insert into person values (38596665,'Armando Stroll','CAN'); --sailor
-insert into person values (10386720,'Albert Eisntein','ARE'); --sailor
-insert into person values (10033958,'Ahmad Salem','ARE'); --sailor
-insert into person values (10049682,'Matias Damasio','MOZ'); --sailor
-insert into person values (58995496,'Nicholas Sparks','GBR'); --sailor
-insert into person values (22059683,'Carlos Sainz','ESP'); --owner
-insert into person values (50025860,'Gregori Mattos','GRC'); --owner
-insert into person values (13359683,'João Silva','PRT'); --owner
-insert into person values (22058689,'John Piastri','ZAF'); --owner & sailor
-insert into person values (20566893,'Lucas Smith','GBR'); --owner & sailor
+--Populate Person (correct)
+insert into person values (126578,'Teresa Villaverde','PT');
+insert into person values (126578,'Martin Scorsese','ZA');
+insert into person values (5679890,'Tim Burton','US');
+insert into person values (456890,'Steven Spielberg','BR');
+insert into person values (88904123,'Quentin Tarantino','AR');
+insert into person values (908765,'Pedro Almodovar','ES');
+insert into person values (14617319,'Christopher Nolan','AU');
+insert into person values (55667,'Hayao Miazaki','JP');
+insert into person values (68904,'Stanley Kubrick','IS');
+insert into person values (1345687,'Federico Fellini','IT');
+insert into person values (2956612,'Marta Silva','PT'); --sailor
+insert into person values (1962857,'Martim Vincente','ES'); --sailor
+insert into person values (4850294,'Carlos Ruiz','AR'); --owner
+insert into person values (1046205,'Diogo Silva','BR'); --owner
+insert into person values (0193758,'Lewis Button','AU'); --sailor
+insert into person values (2957392,'Michael Clifford','AU'); --sailor
+insert into person values (2045869,'Chris Hoeven','AU'); --sailor
+insert into person values (1220588,'Haruki Murakami','JP'); --owner & sailor
+insert into person values (2000582,'Charles Kubica','IS'); --owner
+insert into person values (2945190,'Alfredo Giovinazzi','IT'); --owner
+insert into person values (2945190,'Ana Rendeiro','PT');
+insert into person values (2945191,'João Rendeiro','PT');
+insert into person values (8435738,'Maria Joaquina','PT');
+insert into person values (34678,'Nelson Mandela','ZA');
+insert into person values (27368,'James Cameron','US');
+insert into person values (8873768,'Luana Santana','BR');
+insert into person values (3824,'Pablo Alboran','AR');
+insert into person values (8379268,'Rodrigo Santoro','ES');
+insert into person values (89347874,'Nicole Kidman','AU');
+insert into person values (2934729,'Fusajiro Yamauchi','JP');
+insert into person values (238947,'Jamie Dornan','IS');
+insert into person values (23874876,'Luigi Ferrari','IT');
+insert into person values (38596665,'Armando Stroll','CA'); --sailor
+insert into person values (10386720,'Albert Eisntein','AE'); --sailor
+insert into person values (10033958,'Ahmad Salem','AE'); --sailor
+insert into person values (10049682,'Matias Damasio','MZ'); --sailor
+insert into person values (58995496,'Nicholas Sparks','GB'); --sailor
+insert into person values (22059683,'Carlos Sainz','ES'); --owner
+insert into person values (50025860,'Gregori Mattos','GR'); --owner
+insert into person values (13359683,'João Silva','PT'); --owner
+insert into person values (22058689,'John Piastri','ZA'); --owner & sailor
+insert into person values (20566893,'Lucas Smith','GB'); --owner & sailor
 
 
---Populate Sailor
-insert into sailor values (126578,'PRT');
-insert into sailor values (5679890,'USA');
-insert into sailor values (456890,'BRA');
-insert into sailor values (88904123,'ARG');
-insert into sailor values (908765,'ESP');
-insert into sailor values (2956612,'PRT');
-insert into sailor values (1962857,'ESP');
-insert into sailor values (0193758,'AUS');
-insert into sailor values (2957392,'AUS');
-insert into sailor values (2045869,'AUS');
-insert into sailor values (8435738,'PRT');
-insert into sailor values (34678,'ZAF');
-insert into sailor values (27368,'USA');
-insert into sailor values (8379268,'ESP');
-insert into sailor values (89347874,'AUS');
-insert into sailor values (2934729,'JPN');
-insert into sailor values (238947,'ISL');
-insert into sailor values (23874876,'ITA');
-insert into sailor values (1220588, 'JPN');
-insert into sailor values (38596665,'CAN');
-insert into sailor values (10386720,'ARE');
-insert into sailor values (10033958,'ARE');
-insert into sailor values (10049682,'MOZ');
-insert into sailor values (58995496,'GBR');
-insert into sailor values (22058689,'ZAF');
-insert into sailor values (20566893,'GBR');
+--Populate Sailor (correct)
+insert into sailor values (126578,'PT');
+insert into sailor values (5679890,'US');
+insert into sailor values (456890,'BR');
+insert into sailor values (88904123,'AR');
+insert into sailor values (908765,'ES');
+insert into sailor values (2956612,'PT');
+insert into sailor values (1962857,'ES');
+insert into sailor values (0193758,'AU');
+insert into sailor values (2957392,'AU');
+insert into sailor values (2045869,'AU');
+insert into sailor values (8435738,'PT');
+insert into sailor values (34678,'ZA');
+insert into sailor values (27368,'US');
+insert into sailor values (8379268,'ES');
+insert into sailor values (89347874,'AU');
+insert into sailor values (2934729,'JP');
+insert into sailor values (238947,'IS');
+insert into sailor values (23874876,'IT');
+insert into sailor values (1220588, 'JP');
+insert into sailor values (38596665,'CA');
+insert into sailor values (10386720,'AE');
+insert into sailor values (10033958,'AE');
+insert into sailor values (10049682,'MZ');
+insert into sailor values (58995496,'GB');
+insert into sailor values (22058689,'ZA');
+insert into sailor values (20566893,'GB');
 
---Populate Owner
-insert into owner values (126578,'ZAF','1963-08-01');
-insert into owner values (5679890,'USA','1977-03-31');
-insert into owner values (456890,'BRA','1945-12-05');
-insert into owner values (14617319,'AUS','1974-09-11');
-insert into owner values (55667,'JPN','1961-01-01');
-insert into owner values (68904,'ISL','1953-08-09');
-insert into owner values (1345687,'ITA','1963-08-01');
-insert into owner values (2000582,'ISL', '1999-09-16');
-insert into owner values (2945190,'ITA', '1996-03-01');
-insert into owner values (4850294,'ARG', '1971-10-08');
-insert into owner values (1046205,'BRA', '1985-10-10');
-insert into owner values (8873768,'BRA','1945-01-12');
-insert into owner values (3824,'ARG','1987-02-20');
-insert into owner values (8379268,'ESP','1955-03-11');
-insert into owner values (89347874,'AUS','1965-04-21');
-insert into owner values (2945190,'PRT','1980-07-30');
-insert into owner values (1220588,'JPN','1984-11-30');
-insert into owner values (22059683,'ESP','1954-12-13');
-insert into owner values (50025860,'GRC','1994-09-30');
-insert into owner values (13359683,'PRT','2000-06-23');
-insert into owner values (22058689,'ZAF','2001-04-14');
-insert into owner values (20566893,'GBR','1998-02-17');
-insert into owner values (2945191,'PRT','1955-04-23');
+--Populate Owner (correct)
+insert into owner values (126578,'ZA','1963-08-01');
+insert into owner values (5679890,'US','1977-03-31');
+insert into owner values (456890,'BR','1945-12-05');
+insert into owner values (14617319,'AU','1974-09-11');
+insert into owner values (55667,'JP','1961-01-01');
+insert into owner values (68904,'IS','1953-08-09');
+insert into owner values (1345687,'IT','1963-08-01');
+insert into owner values (2000582,'IS', '1999-09-16');
+insert into owner values (2945190,'IT', '1996-03-01');
+insert into owner values (4850294,'AR', '1971-10-08');
+insert into owner values (1046205,'BR', '1985-10-10');
+insert into owner values (8873768,'BR','1945-01-12');
+insert into owner values (3824,'AR','1987-02-20');
+insert into owner values (8379268,'ES','1955-03-11');
+insert into owner values (89347874,'AU','1965-04-21');
+insert into owner values (2945190,'PT','1980-07-30');
+insert into owner values (1220588,'JP','1984-11-30');
+insert into owner values (22059683,'ES','1954-12-13');
+insert into owner values (50025860,'GR','1994-09-30');
+insert into owner values (13359683,'PT','2000-06-23');
+insert into owner values (22058689,'ZA','2001-04-14');
+insert into owner values (20566893,'GB','1998-02-17');
+insert into owner values (2945191,'PT','1955-04-23');
 
 --Populate Boat
 insert into boat values ('PRT', 98237,2000,40,'O Desconhecido','BRA',8873768);
