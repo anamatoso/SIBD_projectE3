dbcb2d785276e6f75af4909aefe93e504ccc82fd
inessarana <inessarana@gmail.com>
2022-01-21 14:04:17 +0000

    Update ICs.sql

--- ICs.sql
+++ ICs.sql
@@ -11,16 +11,16 @@ BEGIN
     OPEN cursor_reservation;
     LOOP
         FETCH cursor_reservation INTO reserva;
-        if (new.start_date <= reserva.end_date) and (reserva.start_date <= new.end_date)
-            then raise exception 'Reservation_Overlap' using hint ='The reservation overlaps with another on the data base';
+        if (new.start_date <= reserva.end_date) and (new.end_date >= reserva.start_date) then
+            raise exception 'Reservation_Overlap' using hint ='The reservation overlaps with another on the data base';
         end if;
     end loop;
+    return new;
     close cursor_reservation;
 END;
 $$ LANGUAGE plpgsql;
 
-drop trigger tg_insert_reservation on reservation;
-
+--drop trigger tg_insert_reservation on reservation;
 create trigger tg_insert_reservation
 before insert or update ON reservation
 FOR EACH ROW EXECUTE PROCEDURE tg_insert_reservation_proc();
@@ -43,9 +43,9 @@ return new;
 END;
 $$ LANGUAGE plpgsql;
 
-drop trigger tg_check_country on boat;
+--drop trigger tg_check_country on boat;
 CREATE TRIGGER tg_check_country
-before insert or update ON boat
+before insert or update on boat
 FOR EACH ROW EXECUTE PROCEDURE tg_check_country_proc();
 
 
