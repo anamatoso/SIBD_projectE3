248c9b64d445fc6feb4de688358605bc470859eb
Ana Matoso <anamatoso92@gmail.com>
2022-01-23 12:54:23 +0000

    Update ICs.sql

--- ICs.sql
+++ ICs.sql
@@ -22,30 +22,36 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
-DROP TRIGGER tg_insert_reservation ON reservation;
+DROP TRIGGER if exists tg_insert_reservation ON reservation;
 CREATE TRIGGER tg_insert_reservation
 BEFORE INSERT OR UPDATE ON reservation
 FOR EACH ROW EXECUTE PROCEDURE tg_insert_reservation_proc();
 
 --IC2
---pescadinha de rabo na boca
--- código do prof
-DROP TRIGGER IF EXISTS tg_verify_account ON borrower;
+CREATE OR REPLACE FUNCTION verify_location()
+RETURNS TRIGGER AS
+$$
+BEGIN
+    if (select count((NEW.latitude, new.longitude)) from (
+        select latitude, longitude from wharf
+        union all
+        select latitude, longitude from marina
+        union all
+        select latitude, longitude from port
+    ) a --vai unir as 3 tabelas com os repetidos e contar os pares (lat,long) na lista e se for diferente nenhum ou nao esta (=0) ou esta em varios (erro)
+        ) != 1
+        then
+        RAISE EXCEPTION 'The location % is not specialized in only one type of location.', new.name;
+    end if;
+  RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
 
-CREATE CONSTRAINT TRIGGER tg_verify_account
-AFTER INSERT ON borrower DEFERRABLE
-FOR EACH ROW EXECUTE PROCEDURE verify_account();
+DROP TRIGGER IF EXISTS tg_verify_location ON location;
+CREATE CONSTRAINT TRIGGER tg_verify_location
+AFTER INSERT ON location DEFERRABLE
+FOR EACH ROW EXECUTE PROCEDURE verify_location();
 
-START TRANSACTION;
-SET CONSTRAINTS ALL DEFERRED;
-INSERT INTO customer VALUES('Alberto','Rua 1','Lisboa');
-INSERT INTO loan VALUES('1111', 'Central', 100);
--- begin (inserting in borrower before depositor would not be possible without deferring)
-INSERT INTO borrower VALUES('Alberto', '1111');
-INSERT INTO depositor VALUES('Alberto', 'A-101');
--- end (inserting in borrower before depositor would not be possible without deferring)
-COMMIT; -- trigger will be fired here
--- fim do codigo do prof
 
 --IC3
 CREATE OR REPLACE FUNCTION tg_check_country_proc()
@@ -62,7 +68,7 @@ RETURN new;
 END ;
 $$ LANGUAGE plpgsql;
 
-DROP TRIGGER tg_check_country ON boat;
+DROP TRIGGER if exists tg_check_country ON boat;
 CREATE TRIGGER tg_check_country
 BEFORE INSERT OR UPDATE ON boat
 FOR EACH ROW EXECUTE PROCEDURE tg_check_country_proc();
