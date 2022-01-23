582aaf86ef7e51992aac4b53a3f42437421d15c5
inessarana <inessarana@gmail.com>
2022-01-23 10:15:53 +0000

    Update ICs.sql

--- ICs.sql
+++ ICs.sql
@@ -11,20 +11,20 @@ BEGIN
     OPEN cursor_reservation;
     LOOP
         FETCH cursor_reservation INTO reserva;
-        if reserva is null then
-            exit;
-        elsif (new.start_date <= reserva.end_date) and (new.end_date >= reserva.start_date) then
-            raise exception 'Reservation_Overlap' using hint ='The reservation overlaps with another on the data base';
-        end if;
-    end loop;
-    close cursor_reservation;
-    return new;
+        IF reserva IS NULL THEN
+            EXIT;
+        ELSIF (new.start_date <= reserva.end_date) AND (new.end_date >= reserva.start_date) THEN
+            RAISE EXCEPTION 'Reservation_Overlap' USING HINT ='The reservation overlaps with another on the data base';
+        END IF;
+    END LOOP;
+    CLOSE cursor_reservation;
+    RETURN new;
 END;
 $$ LANGUAGE plpgsql;
 
-drop trigger tg_insert_reservation on reservation;
-create trigger tg_insert_reservation
-before insert or update ON reservation
+DROP TRIGGER tg_insert_reservation ON reservation;
+CREATE TRIGGER tg_insert_reservation
+BEFORE INSERT OR UPDATE ON reservation
 FOR EACH ROW EXECUTE PROCEDURE tg_insert_reservation_proc();
 
 --IC2
@@ -51,21 +51,21 @@ COMMIT; -- trigger will be fired here
 CREATE OR REPLACE FUNCTION tg_check_country_proc()
 RETURNS TRIGGER AS
 $$
-begin
-    if (select count(distinct (latitude,longitude)) -- distinct is implicit since lat,long are PK --count(*)
-        from location
-        where iso_code=new.iso_code)<1
-    then
-    raise exception 'Country does not have at least one location';
-    end if;
-return new;
-end;
-$$ language plpgsql;
+BEGIN
+    IF (SELECT count(DISTINCT (latitude,longitude)) -- distinct is implicit since lat,long are PK --count(*)
+        FROM location
+        WHERE iso_code=new.iso_code)<1
+    THEN
+    RAISE EXCEPTION 'Country does not have at least one location';
+    END IF;
+RETURN new;
+END ;
+$$ LANGUAGE plpgsql;
 
-drop trigger tg_check_country on boat;
-create trigger tg_check_country
-before insert or update on boat
-for each row execute procedure tg_check_country_proc();
+DROP TRIGGER tg_check_country ON boat;
+CREATE TRIGGER tg_check_country
+BEFORE INSERT OR UPDATE ON boat
+FOR EACH ROW EXECUTE PROCEDURE tg_check_country_proc();
 
 
 
