bb4a92c775894d490d203ab52e00c819bdc91747
Ana Matoso <anamatoso92@gmail.com>
2022-01-24 16:27:53 +0000

    corrigir query 3

--- queries.sql
+++ queries.sql
@@ -1,22 +1,32 @@
 -- Queries
--- 1. Who is the owner?
+-- 1. Who is the owner with the most boats per country?
 select id_owner, iso_code_owner, iso_code, count(distinct (iso_code, cni))
 from boat
 group by id_owner, iso_code_owner, iso_code
 having count(distinct (iso_code, cni)) >= all(select count(distinct (iso_code, cni)) from boat group by id_owner, iso_code_owner, iso_code);
 
--- 2.
+-- 2. List all the owners that have at least two boats in distinct countries.
 select distinct id_owner, iso_code_owner, count(distinct iso_code) as ncountry
 from boat
 group by id_owner, iso_code_owner
 having count(distinct iso_code) > 1;
 
--- 3. JUSTIFICAR entre PT ou Portugal + ir buscar info as trip em vez de ao sailor (pq a trip tem os sailors que sailam, e nao todos)
+-- 3. Who are the sailors that have sailed to every location in 'Portugal'?
+-- JUSTIFICAR entre PT ou Portugal + ir buscar info as trip em vez de ao sailor (pq a trip tem os sailors que sailam, e nao todos)
 select s.id_sailor, s.iso_code_sailor
 from trip s
 where not exists (
-    (select latitude,longitude from location l where l.iso_code = (select iso_code from country where l.name='Portugal'))
-    except (select end_latitude,end_longitude from trip t where t.id_sailor = s.id_sailor and t.iso_code_sailor = s.iso_code_sailor));
+    (select latitude,longitude
+    from location l
+    where l.iso_code = (select iso_code from country where name='Portugal'))
+    except (
+        select end_latitude, end_longitude
+        from trip t
+        where t.id_sailor = s.id_sailor and t.iso_code_sailor = s.iso_code_sailor
+        union
+        select start_latitude, start_longitude
+        from trip t
+        where t.id_sailor = s.id_sailor and t.iso_code_sailor = s.iso_code_sailor));
 
 -- 4.
 select id_sailor, iso_code_sailor, cni, iso_code_boat, start_date, end_date, count((date, cni, iso_code_boat, id_sailor, iso_code_sailor, start_date, end_date)) as ntrips
