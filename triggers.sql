076bb6b7825760fddb7ef3a779260ef0444cb6e6
inessarana <inessarana@gmail.com>
2022-01-26 10:35:04 +0000

    Update triggers.sql

--- triggers.sql
+++ triggers.sql
@@ -1,6 +1,6 @@
 -- TRIGGERS to ensure the app functions properly
 
--- verify if sailor is already owner (aka in table person) before inserting
+-- verify if sailor/owner is already is person before inserting
 CREATE OR REPLACE FUNCTION chk_person_proc() RETURNS TRIGGER AS
 $$
 BEGIN
@@ -16,31 +16,14 @@ CREATE TRIGGER chk_person
 BEFORE INSERT ON person
 FOR EACH ROW EXECUTE PROCEDURE chk_person_proc();
 
--- old
-CREATE OR REPLACE FUNCTION chk_sailor_proc() RETURNS TRIGGER AS
-$$
-BEGIN
-    IF (NEW.id,NEW.iso_code) NOT IN (SELECT id,iso_code FROM person) THEN
-        INSERT INTO person VALUES(id, name, iso_code);
-    END IF;
-    RETURN NEW;
-END;
-$$ LANGUAGE plpgsql;
-
-CREATE TRIGGER chk_sailor
-BEFORE INSERT ON sailor
-FOR EACH ROW EXECUTE PROCEDURE chk_sailor_proc();
-
-
--- verify if owner is already sailor (aka in table person) before inserting
--- verify if sailor is also owner (aka in table person) before deleting
+-- verify if sailor is also owner before deleting
 CREATE OR REPLACE FUNCTION chk_sailor_del_proc() RETURNS TRIGGER AS
 $$
 BEGIN
     IF (OLD.id,OLD.iso_code) NOT IN (SELECT id,iso_code FROM owner) THEN --not owner
         DELETE FROM person WHERE id=OLD.id AND iso_code=OLD.iso_code;
     END IF;
-    RETURN OLD;
+    RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
 
@@ -49,14 +32,14 @@ CREATE TRIGGER chk_sailor_del
 AFTER DELETE ON sailor
 FOR EACH ROW EXECUTE PROCEDURE chk_sailor_del_proc();
 
--- verify if owner is also sailor (aka in table person) before deleting
+-- verify if owner is also sailor before deleting
 CREATE OR REPLACE FUNCTION chk_owner_del_proc() RETURNS TRIGGER AS
 $$
 BEGIN
     IF (OLD.id,OLD.iso_code) NOT IN (SELECT id,iso_code FROM sailor) THEN --not sailor
         DELETE FROM person WHERE id=OLD.id AND iso_code=OLD.iso_code;
     END IF;
-    RETURN OLD;
+    RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
 
