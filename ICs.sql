91516164d977007a99c7fabdb04fbacad8163121
inessarana <inessarana@gmail.com>
2022-01-29 11:06:10 +0000

    Update ICs.sql

--- ICs.sql
+++ ICs.sql
@@ -1,19 +1,19 @@
 --Integrity Constrains
 
---IC1
+--IC-1: Two reservations for the same boat can not have their corresponding date intervals intersecting.
 CREATE OR REPLACE FUNCTION tg_insert_reservation_proc()
 RETURNS TRIGGER AS
 $$
 DECLARE cursor_reservation CURSOR FOR
     SELECT * FROM reservation;
-declare reserva reservation%ROWTYPE;
+DECLARE reserva reservation%ROWTYPE;
 BEGIN
     OPEN cursor_reservation;
     LOOP
         FETCH cursor_reservation INTO reserva;
         IF reserva IS NULL THEN
             EXIT;
-        ELSIF (new.start_date <= reserva.end_date) AND (new.end_date >= reserva.start_date) and reserva.iso_code_boat=new.iso_code_boat and reserva.cni=new.cni THEN
+        ELSIF (new.start_date <= reserva.end_date) AND (new.end_date >= reserva.start_date) AND reserva.iso_code_boat=new.iso_code_boat AND reserva.cni=new.cni THEN
             RAISE EXCEPTION 'Reservation_Overlap' USING HINT ='The reservation overlaps with another on the data base';
         END IF;
     END LOOP;
@@ -22,29 +22,29 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
-DROP TRIGGER if exists tg_insert_reservation ON reservation;
+DROP TRIGGER IF EXISTS tg_insert_reservation ON reservation;
 CREATE TRIGGER tg_insert_reservation
 BEFORE INSERT OR UPDATE ON reservation
 FOR EACH ROW EXECUTE PROCEDURE tg_insert_reservation_proc();
 
---IC2 verificar mandatory especialization in location
+--IC-2: Any location must be specialized in one of three - disjoint - entities: marina, wharf, or port.
 CREATE OR REPLACE FUNCTION verify_location()
 RETURNS TRIGGER AS
 $$
 BEGIN
-    if (select count(*) from (
-        select latitude, longitude from wharf
-        union all
-        select latitude, longitude from marina
-        union all
-        select latitude, longitude from port) a
-        where latitude=NEW.latitude and longitude=NEW.longitude
+    IF (SELECT count(*) FROM (
+        SELECT latitude, longitude FROM wharf
+        UNION ALL
+        SELECT latitude, longitude FROM marina
+        UNION ALL
+        SELECT latitude, longitude FROM port) a
+        WHERE latitude=new.latitude AND longitude=new.longitude
         --vai unir as 3 tabelas com os repetidos e contar os pares (lat,long) na lista e se for diferente nenhum ou nao esta (=0) ou esta em varios (erro)
         ) <> 1
-        then
+        THEN
         RAISE EXCEPTION 'The location % is not specialized in only one type of location.', new.name;
-    end if;
-  RETURN NEW;
+    END IF;
+  RETURN new;
 END;
 $$ LANGUAGE plpgsql;
 
@@ -54,7 +54,7 @@ AFTER INSERT ON location DEFERRABLE
 FOR EACH ROW EXECUTE PROCEDURE verify_location();
 
 
---IC3
+--IC-3: A country where a boat is registered must correspond - at least - to one location.
 CREATE OR REPLACE FUNCTION tg_check_country_proc()
 RETURNS TRIGGER AS
 $$
@@ -69,7 +69,7 @@ RETURN new;
 END ;
 $$ LANGUAGE plpgsql;
 
-DROP TRIGGER if exists tg_check_country ON boat;
+DROP TRIGGER IF EXISTS tg_check_country ON boat;
 CREATE TRIGGER tg_check_country
 BEFORE INSERT OR UPDATE ON boat
 FOR EACH ROW EXECUTE PROCEDURE tg_check_country_proc();
